52734973b2fc03a54f3dee4d0da3c23d935a8ff6
Mukesh Suthar <sutharmukesh427@gmail.com>
2025-07-06 16:36:41 +0530

    add main.py

--- loops.py
+++ loops.py
@@ -2,6 +2,7 @@ fruits = [ "Apple","Banana","Cherry"]
 for i in fruits:
     print(i+" pie")
 
-for num in range(1,10,2):
-    print(num)
+for num in range(1,10):
+    if num % 2==1:
+     print(num)
 

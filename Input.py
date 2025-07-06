5c9307cea9c9bf4c87beab7d5a8384387a8ed2f0
Mukesh Suthar <sutharmukesh427@gmail.com>
2025-07-06 15:20:42 +0530

    add main.py

--- Input.py
+++ Input.py
@@ -5,4 +5,4 @@ print("Scanning ip is : " + input("What is your ip? "))
 
 fname = input("What is your fname : ")
 lname = input("What is your lname : ")
-print("your name is " + fname+ " "+ lname)
+print("your name is " + fname+ " " + lname)

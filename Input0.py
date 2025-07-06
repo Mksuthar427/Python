bace3261763557dac46ab77a7fff02ba818a870a
Mukesh Suthar <sutharmukesh427@gmail.com>
2025-07-06 15:28:27 +0530

    add main.py

---
+++ Input0.py
@@ -0,0 +1,5 @@
+print("Thanks you for using this program")
+pname = input("What is your name of a pet?")
+bcity = input("What city were you born in? ")
+
+print(f"Your new twitter hanfle and bio: @cyber{pname} from {bcity}")

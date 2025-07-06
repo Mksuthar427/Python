ad72dca8cd16d09819b8c84aa958f637dd292b88
Mukesh Suthar <sutharmukesh427@gmail.com>
2025-07-06 15:49:15 +0530

    add main.py

---
+++ if-statement.py
@@ -0,0 +1,9 @@
+fnum = input("What is first number : ")
+snum = input("What is second number : ")
+
+if fnum > snum:
+ print("first number is greater")
+elif snum > fnum:
+ print("Second number is greater")
+else:
+ print("both are equal")

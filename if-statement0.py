0794209f580d0c8e710491642f90fb952ad54de1
Mukesh Suthar <sutharmukesh427@gmail.com>
2025-07-06 16:15:44 +0530

    add main.py

---
+++ if-statement0.py
@@ -0,0 +1,13 @@
+Score = input(" Enter your Score number : ")
+Score = int(Score)
+
+if Score >= 90:
+ print("Grade A")
+elif Score >= 80 :
+    print( "Grade b")
+elif Score >= 70 :
+    print("Grade C")
+elif Score >= 60 :
+    print( "Grade D")
+else:
+    print("Grade F")

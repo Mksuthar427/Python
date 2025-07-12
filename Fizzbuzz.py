a2c8e8c3e7d8b10bf4561482c828185bad3c3caf
Mukesh Suthar <sutharmukesh427@gmail.com>
2025-07-12 15:54:40 +0530

    add main.py

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -1,5 +1,10 @@
 #Fizbuzz
 for num in range(1,100):
-    if num % 3==0:
-        if num % 5==0:
-         print(num)
+        if num % 3==0 and num % 5==0:
+           print("fizbuzz")
+        elif num % 5==0:
+           print("buzz")
+        elif num % 3==0:
+            print("fizz")
+        else:
+            print(num)
